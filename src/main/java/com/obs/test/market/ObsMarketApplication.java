3686d15f01c8bb3a4739d76db588d1be1de164a3
Musa <musamhtr23@gmail.com>
2024-05-17 21:23:25 +0700

    first commit

---
+++ src/main/java/com/obs/test/market/ObsMarketApplication.java
@@ -0,0 +1,13 @@
+package com.obs.test.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obs/test/market/ObsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obs.test.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
